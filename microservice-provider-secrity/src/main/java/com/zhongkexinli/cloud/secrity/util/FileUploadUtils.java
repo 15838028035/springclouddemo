6d47efa58bd12d02a3df5b1851f2c151440a5fc7
Your Name <you@example.com>
2018-06-28 15:43:57 +0800

    sonar问题修复

--- microservice-provider-secrity/src/main/java/com/zhongkexinli/cloud/secrity/util/FileUploadUtils.java
+++ microservice-provider-secrity/src/main/java/com/zhongkexinli/cloud/secrity/util/FileUploadUtils.java
@@ -1,19 +1,14 @@
 package com.zhongkexinli.cloud.secrity.util;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 
-import com.aliyun.oss.OSSClient;
-import com.aliyun.oss.model.GetObjectRequest;
-import com.aliyun.oss.model.OSSObjectSummary;
-import com.aliyun.oss.model.ObjectListing;
-import com.aliyun.oss.model.ObjectMetadata;
-import com.aliyun.oss.model.PutObjectResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.ObjectMetadata;
+import com.aliyun.oss.model.PutObjectResult;
 
 /**
  * Created by dongxc on 2015/7/20. 文件上传
@@ -45,60 +40,10 @@ public class FileUploadUtils {
             if (result != null) {
                 log.info("文件上传" + result.getETag());
             }
-            // 打印ETag
-            // System.out.println(result.getETag());
         } catch (Exception e) {
             log.error("文件上传失败[FileUploadUtils]", e);
             throw new Exception(e.getMessage(), e);
         }
     }
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        String accessKeyId = "6Horvio4CguKwkTQ";
-        String accessKeySecret = "Y1ACw5BI1WRjiWd8G0OupsQJmpf61z";
-        // 以杭州为例
-        String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
-
-        String bucketName = "weidaiosstest";
-
-        // 初始化一个OSSClient
-        OSSClient client = new OSSClient(endpoint, accessKeyId, accessKeySecret);
-
-        String filePath = "D:/bbb.png";
-        // 获取指定文件的输入流
-        File file = new File(filePath);
-        try {
-            InputStream content = new FileInputStream(file);
-
-            // 创建上传Object的Metadata
-            ObjectMetadata meta = new ObjectMetadata();
-
-            // 必须设置ContentLength
-            meta.setContentLength(file.length());
-
-            // 上传Object.
-            PutObjectResult result = client.putObject(bucketName, "SSOPic", content, meta);
-            // 打印ETag
-            System.out.println(result.getETag());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        // 获取指定bucket下的所有Object信息
-        ObjectListing listing = client.listObjects(bucketName);
-
-        // 遍历所有Object
-        for (OSSObjectSummary objectSummary : listing.getObjectSummaries()) {
-
-            String myKey = objectSummary.getKey();
-            System.out.println(objectSummary.getKey());
-
-            GetObjectRequest getObjectRequest = new GetObjectRequest(bucketName, myKey);
-            System.out.println("uri:" + getObjectRequest.getAbsoluteUri());
-            ObjectMetadata meta = client.getObject(getObjectRequest, new File("D:/c.jpg"));
-
-        }
-
-    }
 }
