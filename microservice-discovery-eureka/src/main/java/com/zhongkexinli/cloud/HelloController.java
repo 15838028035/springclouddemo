d004c3d35a45b65c602e3bbc7cfde39e9e25a6eb
15838028035 <15838028035@163.com>
2018-11-18 18:31:02 +0800

    sonar问题修复

--- microservice-discovery-eureka/src/main/java/com/zhongkexinli/cloud/HelloController.java
+++ microservice-discovery-eureka/src/main/java/com/zhongkexinli/cloud/HelloController.java
@@ -20,7 +20,7 @@ public class HelloController {
 	@RequestMapping("/hello")
 	public String hello(){
 		ServiceInstance serviceInstance = discoveryClient.getInstances("eureka-server").get(0);
-		String infoMsg = "/hello host:{0} ,serviceId:{1} ,port:{2}",serviceInstance.getHost(),serviceInstance.getServiceId(),serviceInstance.getPort();
+		String infoMsg = String.format("/hello host:{0} ,serviceId:{1} ,port:{2}",serviceInstance.getHost(),serviceInstance.getServiceId(),serviceInstance.getPort());
 		logger.info(infoMsg);
 		return infoMsg;
 	}
